8b55e9f78f31c0cafef33918f35ab607658b2e9a
Jordan Taylor <jtaylor81284@hotmail.com>
2024-02-08 21:39:26 +0000

    Refactor for publications

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   Card,
   CardHeader,
@@ -7,53 +8,61 @@ import {
 } from "./ui/card";
 import { Badge } from "./ui/badge";
 
-interface Props {
+interface CardItemProps {
   title: string;
   description: string;
-  tags: readonly string[];
+  tags?: readonly string[];
   link?: string;
+  authors?: string;
+  journal?: string;
+  publicationDate?: string;
+  isPublication?: boolean;
 }
 
-export function ProjectCard({ title, description, tags, link }: Props) {
+export function CardItem({
+  title,
+  description,
+  tags,
+  link,
+  authors,
+  journal,
+  publicationDate,
+  isPublication
+}: CardItemProps) {
   return (
     <Card className="flex flex-col overflow-hidden border border-muted p-3">
       <CardHeader className="">
         <div className="space-y-1">
           <CardTitle className="text-base">
             {link ? (
-              <a
-                href={link}
-                target="_blank"
-                className="inline-flex items-center gap-1 hover:underline"
-              >
-                {title}{" "}
-                <span className="h-1 w-1 rounded-full bg-green-500"></span>
+              <a href={link} target="_blank" className="inline-flex items-center gap-1 hover:underline">
+                {title}
               </a>
             ) : (
               title
             )}
           </CardTitle>
-          <div className="hidden font-mono text-xs underline print:visible">
-            {link?.replace("https://", "").replace("www.", "").replace("/", "")}
+          {isPublication && (
+            <>
+              <CardDescription className="font-mono text-xs">
+                {authors}
+              </CardDescription>
+              <CardDescription className="font-mono text-xs">
+                {journal} ({publicationDate})
+              </CardDescription>
+            </>
+          )}
+            <CardDescription className="font-mono text-xs">{description}</CardDescription>
+          <div className="mt-2 flex flex-wrap gap-1">
+            {tags?.map((tag) => (
+              <Badge className="px-1 py-0 text-[10px]" variant="secondary" key={tag}>
+                {tag}
+              </Badge>
+            ))}
           </div>
-          <CardDescription className="font-mono text-xs">
-            {description}
-          </CardDescription>
         </div>
       </CardHeader>
-      <CardContent className="mt-auto flex">
-        <div className="mt-2 flex flex-wrap gap-1">
-          {tags.map((tag) => (
-            <Badge
-              className="px-1 py-0 text-[10px]"
-              variant="secondary"
-              key={tag}
-            >
-              {tag}
-            </Badge>
-          ))}
-        </div>
-      </CardContent>
     </Card>
   );
+
 }
