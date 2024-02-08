d4c15450aad996ffde3e5b95240c1fd4c5021965
Jordan Taylor <jtaylor81284@hotmail.com>
2024-02-08 22:15:34 +0000

    Make cmd platform agnostic

--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import * as React from "react";
-import { Calculator, Calendar, Smile } from "lucide-react";
 
 import {
   CommandDialog,
@@ -37,7 +36,7 @@ export const CommandMenu = ({ links }: Props) => {
       <p className="fixed bottom-0 left-0 right-0 border-t border-t-muted bg-white p-1 text-center text-sm text-muted-foreground print:hidden">
         Press{" "}
         <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-          <span className="text-xs">⌘</span>J
+          <span className="text-xs">[Ctrl/⌘] + </span>J
         </kbd>{" "}
         to open the command menu
       </p>
